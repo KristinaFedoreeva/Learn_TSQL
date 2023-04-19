2dc81190bdbf00518dea7dad3f9596e8edc9daf4
KristinaFedoreeva <cfedoreeva@gmail.com>
2023-04-19 16:47:31 +0500

    Add GetSKUPrice function

---
+++ dbo.udf_GetSKUPrice.sql
@@ -0,0 +1,12 @@
+CREATE FUNCTION dbo.udf_GetSKUPrice (@ID_SKU INT)
+RETURNS DECIMAL(18, 2)
+AS
+BEGIN
+    DECLARE @AvgPrice DECIMAL(18, 2);
+
+    SELECT @AvgPrice = SUM(Value) / SUM(Quantity)
+    FROM dbo.Basket
+    WHERE ID_SKU = @ID_SKU;
+
+    RETURN @AvgPrice;
+END;
